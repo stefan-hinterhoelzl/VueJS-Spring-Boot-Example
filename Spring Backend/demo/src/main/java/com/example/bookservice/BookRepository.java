28419cca8f22b1118f7f057fc83216f64810653d
stefan-hinterhoelzl <59830330+stefan-hinterhoelzl@users.noreply.github.com>
2021-05-31 08:17:07 +0200

    repo changes

--- Spring Backend/demo/src/main/java/com/example/bookservice/BookRepository.java
+++ Spring Backend/demo/src/main/java/com/example/bookservice/BookRepository.java
@@ -5,12 +5,12 @@ import java.util.List;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends CrudRepository<Book, Long> {
     List<Book> findByTitle(String title);
 
     @Query("Select b From Books Where LOWER(author) = LOWER(:author)")
     List<Book> findByAuthor(@Param("author") String author);
-
-
 }
