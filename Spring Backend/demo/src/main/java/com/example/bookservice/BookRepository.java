dde7946301dcf1a7195912cff02d903478a798fd
stefan-hinterhoelzl <59830330+stefan-hinterhoelzl@users.noreply.github.com>
2021-05-31 15:06:31 +0200

    jpa fix

--- Spring Backend/demo/src/main/java/com/example/bookservice/BookRepository.java
+++ Spring Backend/demo/src/main/java/com/example/bookservice/BookRepository.java
@@ -2,15 +2,14 @@ package com.example.bookservice;
 
 import java.util.List;
 
-import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends CrudRepository<Book, Long> {
     List<Book> findByTitle(String title);
 
-    @Query("Select b From Books Where LOWER(author) = LOWER(:author)")
-    List<Book> findByAuthor(@Param("author") String author);
+    @Query(value = "Select * From Books Where LOWER(author) = LOWER(?1)", nativeQuery = true)
+    List<Book> findByAuthor(String author);
 }
